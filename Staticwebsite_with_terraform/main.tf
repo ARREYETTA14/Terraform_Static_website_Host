125f1a2d6626aca2de857ec49b554f1d035c6663
Arreyetta Bawak Augustine Besong <112652000+ARREYETTA14@users.noreply.github.com>
2025-05-06 23:25:25 +0100

    Update main.tf

--- Staticwebsite_with_terraform/main.tf
+++ Staticwebsite_with_terraform/main.tf
@@ -1,6 +1,9 @@
+
+########### Creating S3 Bucket ############
+
 ## Create Bucket
 resource "aws_s3_bucket" "static-website-hoster" {
-  bucket = "my-static-website-hoster"
+  bucket = "my-static-website-hoster134"
   tags = {
     Name        = var.bucket_name
   }
@@ -15,7 +18,7 @@ resource "aws_s3_bucket_ownership_controls" "example" {
   bucket = aws_s3_bucket.static-website-hoster.id
 
   rule {
-    object_ownership = "BucketOwnerPreferred"
+    object_ownership = "BucketOwnerEnforced"
   }
 }
 
@@ -29,44 +32,78 @@ resource "aws_s3_bucket_public_access_block" "example" {
   restrict_public_buckets = false
 }
 
-## Bucket ACLs
-resource "aws_s3_bucket_acl" "Acl" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.example,
-    aws_s3_bucket_public_access_block.example,
-  ]
-
+## bucket policy to allow public access to the bucket
+resource "aws_s3_bucket_policy" "public_access" {
   bucket = aws_s3_bucket.static-website-hoster.id
-  acl    = "public-read"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.static-website-hoster.arn}/*"
+      }
+    ]
+  })
 }
+
+
 ######################################################################################################
 # End of policy block
 ######################################################################################################
 
-## Upload objects in bucket
+############## Uploading upjects ###############
+
 # uploading index.html file
 resource "aws_s3_object" "index" {
   bucket = aws_s3_bucket.static-website-hoster.id
   key    = "index.html"
   source = "./index.html" ## If found in same directory, just give name .e.g index.html
-  acl = "public-read"
-  # content_type = "text/html"
+  content_type = "text/html"
 }
 #uploading error.html file
 resource "aws_s3_object" "error" {
   bucket = aws_s3_bucket.static-website-hoster.id
   key    = "error.html"
-  source = "./error.html" ## If found in same directory, just give name .e.g error.html
-  acl = "public-read"
-  # content_type = "text/html"
+  source = "./error.html" 
+  content_type = "text/html"
 }
 
-##upload picture to be be used by the index.html file
-resource "aws_s3_object" "profile_picture" {
+##upload picture to be used by the index.html file
+
+##image one
+resource "aws_s3_object" "profile_picture1" {
   bucket = aws_s3_bucket.static-website-hoster.id
-  key    = "profile.jpg"
-  source = "./profile.jpg" ## If found in same directory, just give name .e.g profile.jpg
-  acl = "public-read"
+  key    = "cameroonian-dish-1.jpg"
+  source = "./cameroonian-dish-1.jpg"
+  content_type = "image/jpg"
+}
+
+##image two
+resource "aws_s3_object" "profile_picture2" {
+  bucket = aws_s3_bucket.static-website-hoster.id
+  key    = "cameroonian-dish-2.jpg"
+  source = "./cameroonian-dish-2.jpg" 
+  content_type = "image/jpg"
+}
+
+## image three
+resource "aws_s3_object" "profile_picture3" {
+  bucket = aws_s3_bucket.static-website-hoster.id
+  key    = "Kenyan-dish-1.jpg"
+  source = "./Kenyan-dish-1.jpg" 
+  content_type = "image/jpg"
+}
+
+## image four
+resource "aws_s3_object" "profile_picture4" {
+  bucket = aws_s3_bucket.static-website-hoster.id
+  key    = "Kenyan-dish-2.jpg"
+  source = "./Kenyan-dish-2.jpg" 
+  content_type = "image/jpg"
 }
 
 ## Renders S3 bucket a host for static website 
@@ -74,15 +111,15 @@ resource "aws_s3_bucket_website_configuration" "website" {
   bucket = aws_s3_bucket.static-website-hoster.id
 
   index_document {
-    suffix = var.index_document_suffix
+    suffix = "index.html"
   }
 
   error_document {
-    key = var.error_document
+    key = "error.html"
   }
 
-depends_on = [ aws_s3_bucket_acl.Acl ]
+depends_on = [ aws_s3_bucket_policy.public_access ]
 }
 
-## Note that, you can as well upload your css. files 
-## As well as all other photos that will be usedby the index.html file
+## Note that you can also upload your CSS. files 
+## As well as all other photos that will be used by the index.html file
