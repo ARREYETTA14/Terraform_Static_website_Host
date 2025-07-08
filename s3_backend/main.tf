45958f4d004571aa565b1a98d95a8c796faa9cff
Arreyetta Bawak Augustine Besong <112652000+ARREYETTA14@users.noreply.github.com>
2025-07-08 11:39:22 +0100

    Update main.tf

--- s3_backend/main.tf
+++ s3_backend/main.tf
@@ -3,7 +3,7 @@
 # resource "aws_s3_bucket" "tf_state_bucket" {
 #   bucket = "my-terraform-backend-bucket1234"
 
-    force_destroy = true
+#    force_destroy = true
 
 #   tags = {
 #     Name = "Terraform State Bucket"
